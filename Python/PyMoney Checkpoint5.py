093c90f35217561fdb1090b41601b74842e5cd2a
Ethane Hsieh <ethane1755@gapp.nthu.edu.tw>
2024-10-07 17:02:01 +0800

    add_comments

--- Python/PyMoney Checkpoint5.py
+++ Python/PyMoney Checkpoint5.py
@@ -1,15 +1,16 @@
 balance = int(input('How much money do you have?')) 
 record = []
-i = 0
-j = 1
 
 
 while True:
-    change = str(input('Add some expense or income records with description and amount:'))
+    change = str(input('Add some expense or income records with description and amount:')) 
+    #set breakpoint
     if change == 'quit':
         print('Thank you.')
         break
+    #split the diffrent entries
     change = str.split(change,',')
+    #split the description and money in each entry
     for entry in change:
         task, num = str.split(entry)
         num = int(num)
