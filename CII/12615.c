4ee93d923c653c3e8027c7c47a4767f83df3dbcd
Ethane Hsieh <ethane1755@gapp.nthu.edu.tw>
2025-02-03 23:29:04 +0800

    Refactor DFS function and clean up comments in knight's tour program

--- CII/12615.c
+++ CII/12615.c
@@ -5,7 +5,6 @@
 #define TARGET "ICPCASIASG"
 #define TARGET_LEN 10
 
-// 騎士的 8 種移動方式
 const int knight_moves[8][2] = {
     {-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, 
     {1, -2}, {1, 2}, {2, -1}, {2, 1}
@@ -14,17 +13,16 @@ const int knight_moves[8][2] = {
 int N;
 char chess[MAX_N][MAX_N];
 
-// 深度優先搜尋 (DFS)，檢查是否可以找到目標字串
 int dfs(int x, int y, int index) {
-    if (index == TARGET_LEN) return 1; // 成功找到完整字串
+    if (index == TARGET_LEN) return 1; 
 
     for (int i = 0; i < 8; i++) {
         int nx = x + knight_moves[i][0];
         int ny = y + knight_moves[i][1];
 
-        if (nx >= 0 && nx < N && ny >= 0 && ny < N) { // 確保在範圍內
+        if (nx >= 0 && nx < N && ny >= 0 && ny < N) { 
             if (chess[nx][ny] == TARGET[index]) { 
-                if (dfs(nx, ny, index + 1)) return 1; // 繼續搜尋
+                if (dfs(nx, ny, index + 1)) return 1; 
             }
         }
     }
@@ -37,14 +35,12 @@ int main() {
     char S[MAX_N * MAX_N + 1];
     scanf("%s", S);
 
-    // 將 S 轉換為 N×N 的棋盤
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             chess[i][j] = S[i * N + j];
         }
     }
 
-    // 從所有 'I' 開始 DFS
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             if (chess[i][j] == 'I') {
