21207993a045cf4041e8c06003ebb65c18424494
Ethane Hsieh <ethane1755@gapp.nthu.edu.tw>
2025-04-10 17:47:46 +0800

    Implement parent and child relationship methods in Person class

--- CII/CPP/13862.cpp
+++ CII/CPP/13862.cpp
@@ -2,109 +2,103 @@
 #include "13862.h"
 using namespace std;
 
-string* ParseStatement(string statement) { // return the parsed array
-    stringstream ss;
-    vector<string> v;
-    ss << statement;
-    while (ss >> statement) v.push_back(statement);
-    string* ret = new string[v.size()];
-    for (int _ = 0; _ < v.size(); ++_) {
-        ret[_] = v[_];
+void ParentA(Person* p) {
+    if (!p -> parentA) {
+        p -> parentA = new Person();
+        p -> parentA -> child = p;
+        if (p -> parentB) {
+            p -> parentA -> mate = p -> parentB;
+            p -> parentB -> mate = p -> parentA;
+        }
     }
-    return ret;
 }
 
-int getParseStatementLength(string statement) {
-    stringstream ss;
-    ss << statement;
-    int ret = 0;
-    while (ss >> statement) ++ret;
-    return ret;
+void ParentB(Person* p) {
+    if (!p -> parentB) {
+        p -> parentB = new Person();
+        p -> parentB -> child = p;
+        if (p -> parentA) {
+            p -> parentA -> mate = p -> parentB;
+            p -> parentB -> mate = p -> parentA;
+        }
+    }
+}
+
+void Mate(Person* p) {
+    if (!p -> mate) {
+        p -> mate = new Person();
+        p -> mate -> mate = p;
+        if (p -> child) {
+            if (p -> child -> parentA == p) {
+                p -> child -> parentB = p -> mate;
+                p -> mate -> child = p -> child;
+            } else if (p -> child -> parentB == p) {
+                p -> child -> parentA = p -> mate;
+                p -> mate -> child = p -> child;
+            }
+        }
+    }
+}
+
+void Child(Person* p) {
+    if (!p -> child) {
+        p -> child = new Person();
+        p -> child -> parentA = p;
+        if (p -> mate) {
+            p -> child -> parentB = p -> mate;
+            p -> mate -> child = p -> child;
+        }
+    }
 }
 
 Person* Person::getRelative (string* arr, int now, int len){
-    Person* cur = this;
-    for (int i = now; i < len; i++) {
-        if (arr[i] == 'ParentA') {
-            if (cur -> parentA == nullptr) {
-                cur -> parentA = new Person();
-                cur -> parentA -> child = cur;
-                if (cur -> parentB) {
-                    cur -> parentB -> mate = cur -> parentA;
-                    cur -> parentA -> mate = cur -> parentB;
-                }
-            }
-            else cur = cur -> parentA;
-        } else if (arr[i] == 'ParentB') {
-            if (cur -> parentB == nullptr) {
-                cur -> parentB = new Person();
-                cur -> parentB -> child = cur;
-                if (cur -> parentA) {
-                    cur -> parentB -> mate = cur -> parentA;
-                    cur -> parentA -> mate = cur -> parentB;
-                }
-            }
-            else cur = cur -> parentB;
-        } else if (arr[i] == 'Mate') {
-            if (cur -> mate) {
-                cur = cur -> mate;
-            }
-            else {
-                cur -> mate = new Person();
-                cur -> mate -> mate = cur;
-                if (cur -> child) {
-                    if (cur -> child -> parentA == cur) cur -> child -> parentB = cur -> mate;
-                    else cur -> child -> parentA = cur -> mate;
-                    cur -> mate -> child = cur -> child;
-                }
-            }
-        } else if (arr[i] == 'Child') {
-            if (cur -> child == nullptr) {
-                cur -> child = new Person();
-                cur -> child -> parentA = cur;
-                if (cur -> mate) {
-                    cur -> mate -> child = cur -> child;
-                    cur -> child -> parentB = cur -> mate;
-                }
-            }
-            else cur = cur -> child;
-        }
-    }
-    return cur;
+    if (now == len) return this;
+    if (arr[now] == "ParentA") {
+        ParentA(this);
+        return this -> parentA -> getRelative(arr, now + 1, len);
+    } else if (arr[now] == "ParentB") {
+        ParentB(this);
+        return this -> parentB -> getRelative(arr, now + 1, len);
+    } else if (arr[now] == "Mate") {
+        Mate(this);
+        return this -> mate -> getRelative(arr, now + 1, len);
+    } else if (arr[now] == "Child") {
+        Child(this);
+        return this -> child-> getRelative(arr, now + 1, len);
+    } else return this;
 }
 
-int main () {
-    Person* narrator = new Person();
-
-    narrator->name = "Pineapple Senpai";
-    narrator->age = 21;
-    narrator->gender = MALE;
-    narrator->personality = "seasick";
-
-    int Q;
-    cin >> Q;
-    for (int _ = 0; _ < Q; ++_) {
-        // input type of the query
-        int type;
-        cin >> type;
-        // get the parsed array
-        string statement;
-        getline(cin, statement);
-        string* arr = ParseStatement(statement);
-        // compute the length of the array
-        int len = getParseStatementLength(statement);
-        switch(type) {
-            case 1: 
-                narrator = narrator->getRelative(arr, 0, len);
-                break;
-            case 2: 
-                narrator->describe(arr, 0, len);
-                break;
-            case 3:
-                cout << narrator->info() << "\n";
-                break;
-        }
-        delete[] arr;
+void Person::describe(string* arr, int now, int len) {
+    if (now >= len) return;
+    if (arr[now] == "ParentA") {
+        ParentA(this);
+        //printf("%s", this -> parentA -> info());
+        this -> parentA -> describe(arr, now + 1, len);
+    } else if (arr[now] == "ParentB") {
+        ParentB(this);
+        //printf("%s", this -> parentB -> info());
+        this -> parentB -> describe(arr, now + 1, len);
+    } else if (arr[now] == "Mate") {
+        Mate(this);
+        //printf("%s", this -> mate -> info());
+        this -> mate -> describe(arr, now + 1, len);
+    } else if (arr[now] == "Child") {
+        Child(this);
+        //printf("%s", this -> child -> info());
+        this -> child -> describe(arr, now + 1, len);
+    } else if (arr[now] == "Gender") {
+        if (arr[now + 1] == "MALE") this->gender = MALE;
+        else this->gender = FEMALE;
+        this->describe(arr, now + 2, len);
+    } else if (arr[now] == "Age") {
+        this->age = stoi(arr[now + 1]);
+        this->describe(arr, now + 2, len);
+    } else if (arr[now] == "Name") {
+        this->name = arr[now + 1];
+        this->describe(arr, now + 2, len);
+    } else if (arr[now] == "Personality") {
+        if (this->personality == "") this->personality = arr[now + 1];
+        else this->personality += " " + arr[now + 1];
+        this->describe(arr, now + 2, len);
     }
-    return 0;
 }
