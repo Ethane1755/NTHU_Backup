595a391e325da373c63d0a78fa8b53c344489ce6
Ethane1755 <87981364+Ethane1755@users.noreply.github.com>
2025-07-15 23:13:58 +0800

    CII: Add new executable and update test files

--- CII/test.cpp
+++ CII/test.cpp
@@ -1,19 +1,10 @@
 #include <iostream>
-#include <fstream>
-#include <ctime>
-#include <string>
-#include <iomanip>
+
+using namespace std;
 
 int main() {
-	std::string playerName = "Player1";
-	int score = 100;
-
-	auto t = std::time(nullptr);
-	auto tm = *std::localtime(&t);
-
-	std::ofstream output("test.txt", std::ios::app);
-	output << playerName << " " << score << " " << std::put_time(&tm, "%Y%m%d %H%M%S") << std::endl;
-	output.close();
+	cout << "3";
 
 	return 0;
 }
+
