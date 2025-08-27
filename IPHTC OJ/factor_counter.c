f889e351cdf3af9e2f4384b58bf91492927e31dd
Ethane1755 <87981364+Ethane1755@users.noreply.github.com>
2025-08-27 21:56:04 +0800

    11/163

---
+++ IPHTC OJ/factor_counter.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>
+
+int arr[20000];
+
+int find_factor(int a) {
+    int cnt = 0;
+    for (int i = 1; i <= sqrt(a); i++) {
+        if (a % i == 0 && i != sqrt(a)) cnt += 2;
+        else if (a % i == 0 && i == sqrt(a)) cnt += 1;
+    }
+    return cnt;
+}
+
+int main () {
+    int T, l, r;
+    int temp, ans;
+    for (int i = 1; i <= 20000; i++) {
+        arr[i-1] = find_factor(i);
+    }
+    scanf("%d", &T);
+    while (T--) {
+        temp = 0;
+        scanf("%d %d", &l, &r);
+        if (l == r) {
+            printf("%d\n", l);
+        } else {
+            for (int j = l-1; j < r; j++) {
+                printf("%d %d %d\n", j+1, arr[j], temp);
+                if (arr[j] > temp) {
+                    temp = arr[j];
+                    ans = j+1;
+                }
+            }
+            printf("%d\n", ans);
+        }
+    }
+
+    return 0;
+}
---
+++ IPHTC OJ/get_num.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main () {
+    int target;
+    //int a, b;
+    scanf("%d", &target);
+    printf("%d %d\n", target+4, 4);
+
+    return 0;
+}
