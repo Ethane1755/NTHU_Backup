c302056a6963a7ed012b4444074efa070e124087
Ethane Hsieh <87981364+Ethane1755@users.noreply.github.com>
2025-09-09 14:49:10 +0800

    finished DS hw1

--- 114-1/DS/spiral.cpp
+++ 114-1/DS/spiral.cpp
@@ -3,15 +3,17 @@
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int n, N, M, Q, L, R;
     cin >> n;
     while (n--) {
         cin >> N >> M >> Q;
         vector<vector<int>> arr(N, vector<int>(M));
-        vector<vector<int>> visited(N, vector<int>(M, 0));
-        int* prefix = (int*) malloc (sizeof (int) * N * M);
+        vector<vector<bool>> visited(N, vector<bool>(M, false));
+        vector<long long> prefix (N*M+1, 0);
         int ind = 1;
-        prefix[0] = 0;
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
@@ -26,25 +28,28 @@ int main() {
         for (int step = 0; step < N * M; step++) {
             prefix[ind] = prefix[ind - 1] + arr[x][y];
             ind++;
-            visited[x][y] = 1;
-            int nx = x + dx[dir];
-            int ny = y + dy[dir];
-            if (nx >= 0 && nx < N && ny >= 0 && ny < M && !visited[nx][ny]) {
+            visited[x][y] = true;
+
+            if (step < N * M - 1) {
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M || visited[nx][ny]) {
+                    dir = (dir + 1) % 4;
+                    nx = x + dx[dir];
+                    ny = y + dy[dir];
+                }
                 x = nx;
                 y = ny;
-            } else {
-                dir = (dir + 1) % 4;
-                x += dx[dir];
-                y += dy[dir];
             }
         }
 
         while (Q--) {
             cin >> L >> R;
-            int ans = prefix[R] - prefix[L - 1];
-            cout << ans << endl;
+            long long ans = prefix[R] - prefix[L - 1];
+            cout << ans << '\n';
         }
-        free(prefix);
     }
     
 }
+
